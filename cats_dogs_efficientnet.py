a8eff3fdb1dc3bd18fd26bd6abd5e44c3d9035b8
sanjeeb subedi <sanjeebsubedi4@gmail.com>
2021-02-10 19:24:47 +0545

    Moved most of the hyperparameters and variables to runtimeVariables.py

--- cats_dogs_efficientnet.py
+++ cats_dogs_efficientnet.py
@@ -42,12 +42,9 @@ def train_model(train_loader, model, optimizer, num_epochs):
     for batch in train_loader:
       images = batch[0].to(device)
       labels = batch[1].to(device)
-      # print(labels)
-      # images = images.cuda()
       optimizer.zero_grad()
   
       preds = model(images)
-      # print(preds.argmax(dim=1))
       loss = F.cross_entropy(preds, labels)
   
       loss.backward()
@@ -65,45 +62,27 @@ def validate_model(model, loader):
     for images,labels in loader:
       images = images.to(device)
       labels = labels.to(device)
-      # print(labels)
       preds = model(images)
-      # print(f'preds: {preds.argmin(dim=1)}')
       num_correct += get_num_correct(preds, labels)
-      # print(num_correct)
     
     print(f'Predictions accuracy: {num_correct}')
 
-
-#Hyperparameters and paths
-main_dir = 'drive/MyDrive/kaggle/dogs_vs_cats/train'
-image_size = 224
-rgb_mean = [0.485, 0.456, 0.406]
-rgb_std = [0.229, 0.224, 0.225]
-train_split = 0.8
-num_workers = multiprocessing.cpu_count()
-model_name = 'efficientnet-b0'
-training_epochs = 10
-learning_rate = 0.0001
-model_save_path = 'drive/MyDrive/dogs_vs_cats/models/cats_dogs_efficientnet.pt'
-batch_size = 100
-
-
-transform_list = [transforms.ToPILImage(), transforms.Resize(image_size),
-                  transforms.ToTensor(),transforms.Normalize(rgb_mean, rgb_std)]
+transform_list = [transforms.ToPILImage(), transforms.Resize(vars.image_size),
+                  transforms.ToTensor(),transforms.Normalize(vars.rgb_mean, vars.rgb_std)]
 
 print('Setting device...')
 set_device()
 print('Creating training and validation dataloaders...')
-train_loader, valid_loader = load_data(main_dir, transformations=transform_list,
-                                       batch_size=batch_size, train_split=train_split)
+train_loader, valid_loader = load_data(vars.main_dir, transformations=transform_list,
+                                       batch_size=vars.batch_size, train_split=vars.train_split)
 print('Defining model')
-model = get_model(model_name, num_of_classes=2, input_channels=3, image_size=image_size)
+model = get_model(vars.model_name, num_of_classes=2, input_channels=3, image_size=vars.image_size)
 print('Initializing the optimizer...')
-optimizer = adam(model, learning_rate)
+optimizer = adam(model, vars.learning_rate)
 print('Training the model...')
 trained_model = train_model(train_loader, model, optimizer,
-                           num_epochs=training_epochs)
+                           num_epochs=vars.training_epochs)
 print('Saving the model...')
-torch.save(trained_model, model_save_path)
+torch.save(trained_model, vars.model_save_path)
 print('validating the model...')
 validate_model(trained_model, valid_loader)
---
+++ runtimeVariables.py
@@ -0,0 +1,10 @@
+main_dir = 'drive/MyDrive/kaggle/dogs_vs_cats/train'
+image_size = 224
+rgb_mean = [0.485, 0.456, 0.406]
+rgb_std = [0.229, 0.224, 0.225]
+train_split = 0.8
+model_name = 'efficientnet-b0'
+training_epochs = 10
+learning_rate = 0.0001
+model_save_path = 'drive/MyDrive/dogs_vs_cats/models/'
+batch_size = 100
