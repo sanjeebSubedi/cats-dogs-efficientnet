5b57b770bb7f0a2c22fcfbe192a36232a28ad44d
sanjeeb subedi <sanjeebsubedi4@gmail.com>
2021-02-15 14:21:14 +0545

    added more HPs and variables

--- runtimeVariables.py
+++ runtimeVariables.py
@@ -1,12 +1,20 @@
 import multiprocessing
+from efficientnet_pytorch import EfficientNet
+import os
+
 num_workers = multiprocessing.cpu_count()
-main_dir = 'drive/MyDrive/kaggle/dogs_vs_cats/train'
-image_size = 224
+main_dir = '/content/drive/MyDrive/local_leaves/train'
+model_name = 'efficientnet-b0'
+image_size = EfficientNet.get_image_size(model_name)
 rgb_mean = [0.485, 0.456, 0.406]
 rgb_std = [0.229, 0.224, 0.225]
 train_split = 0.8
-model_name = 'efficientnet-b0'
 training_epochs = 10
 learning_rate = 0.0001
-model_save_path = 'drive/MyDrive/dogs_vs_cats/models/'
-batch_size = 100
+checkpoint_dir = '/content/drive/MyDrive/local_leaves/models'
+batch_size = 32
+checkpoint_save_frequency = 2
+checkpoint_filename = model_name + '.pth.tar'
+load_pretrained_weights = False
+checkpoint_path = os.path.join(checkpoint_dir, checkpoint_filename)
+load_checkpoint = True
